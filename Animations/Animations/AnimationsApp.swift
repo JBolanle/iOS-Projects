40f755f60fd6ec165ef9aae2ee5bf81c8d1cfa88
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-04-05 01:40:01 -0500

    init
    starting at project 6 (animations) from hacking with ios, swiftui edition by paul hudson. created xcode project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jumoke Bolanle on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
