f1caf88514cfcaea98e6efa0b500a98334812365
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-04-10 20:14:06 -0500

    animation order lesson

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -17,7 +17,10 @@ struct ContentView: View {
         }
         .frame(width: 200, height: 200)
         .background(enabled ? .blue : .red)
+        .animation(.default, value: enabled)
         .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
     }
     
     struct ContentView_Previews: PreviewProvider {
