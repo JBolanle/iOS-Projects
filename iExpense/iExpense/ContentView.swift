0faae343347a53a895b67e8b0e97dad3b3e16b15
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-04-19 01:12:16 -0500

    learning about userdefaults, sheets and multiple views

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -9,18 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var user = User()
-    
+    @State private var showingSheet = false
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-            
-            
+//        Button("Show Sheet") {
+//            showingSheet.toggle()
+//
+//        }
+//        .sheet(isPresented: $showingSheet) {
+//            secondView()
+//        }
+        
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
+            UserDefaults.standard.set(self.tapCount, forKey: "Tap")
         }
         
+    }
+}
+
+struct secondView: View {
+    @Environment (\.dismiss) var dismiss
+    
+    @State private var numbers = [Int]()
+    @State private var currentNumbers = 1
+    
+//    let name: String
+    
+    var body: some View {
+//        Text("Hello, \(name)!")
+//        Button("Dismiss") {
+//            dismiss()
+//        }
+        
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumbers)
+                    currentNumbers += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+
     }
 }
 
