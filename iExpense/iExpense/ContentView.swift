78c7431123e46c954a499a99d48680ff95c588fe
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-04-23 15:45:01 -0500

    created new view for user to add expense item properties

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    
+    @State private var showingAddExpenses = false
     
     var body: some View {
-        Section {
-            
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpenses = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpenses) {
+                AddView(expenses: expenses)
+            }
         }
     }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
