f725dceac0fcaa70f9247596ca30f6e2c0a49caa
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-04-30 15:33:37 -0500

    init Moonshot project

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jumoke Bolanle on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
