2c033ab3aa88fb8d83b0b0d2cfc3cbb3ad289eec
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-05-06 19:52:33 -0500

    init Drawing xcode project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jumoke Bolanle on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
