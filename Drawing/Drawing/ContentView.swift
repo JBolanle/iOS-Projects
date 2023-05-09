db6d818e24d3b9d569bcd2b3218c815711414b64
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-05-09 16:31:51 -0500

    messing with blur and saturation. demonstrating real time rendering

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
+    @State private var amount = 0.0
     
     
     var body: some View {
         VStack {
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
+            ZStack {
+                Image("PaulHudson")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .saturation(amount)
+                    .blur(radius: (1 - amount) * 20)
+                
+//                Circle()
+//                    .fill(Color(red: 1, green: 0, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: -50, y: -80)
+//                    .blendMode(.screen)
+//                
+//                Circle()
+//                    .fill(Color(red: 0, green: 1, blue: 0))
+//                    .frame(width: 200 * amount)
+//                    .offset(x: 50, y: -80)
+//                    .blendMode(.screen)
+//                
+//                Circle()
+//                    .fill(Color(red: 0, green: 0, blue: 1))
+//                    .frame(width: 200 * amount)
+//                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
             
-            Slider(value: $colorCycle)
+            Slider(value: $amount)
+                .padding()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
     }
 }
 
