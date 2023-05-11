7dd44e725a14bb19f7e307b7c49acf89d106af42
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-05-11 14:14:02 -0500

    working with AnimatableData and AnimatablePair

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,44 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var amount = 0.0
-    
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
-        VStack {
-            ZStack {
-                Image("PaulHudson")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .saturation(amount)
-                    .blur(radius: (1 - amount) * 20)
-                
-//                Circle()
-//                    .fill(Color(red: 1, green: 0, blue: 0))
-//                    .frame(width: 200 * amount)
-//                    .offset(x: -50, y: -80)
-//                    .blendMode(.screen)
-//                
-//                Circle()
-//                    .fill(Color(red: 0, green: 1, blue: 0))
-//                    .frame(width: 200 * amount)
-//                    .offset(x: 50, y: -80)
-//                    .blendMode(.screen)
-//                
-//                Circle()
-//                    .fill(Color(red: 0, green: 0, blue: 1))
-//                    .frame(width: 200 * amount)
-//                    .blendMode(.screen)
+        CheckerBoard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
+                }
             }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
+    }
+    
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+    
+}
+
+struct CheckerBoard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // figure out how big each row/column should be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        // loop over all rows/columns and make alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // makes sense. this square should be colored
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.black)
-        .ignoresSafeArea()
+        
+        return path
+    }
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
     }
 }
 
