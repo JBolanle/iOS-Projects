619448a703e5a19ff6e784708841f94f3804795d
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-05-15 16:04:10 -0500

    set up navigation link to vew habit detail

--- Habittude Habit Tracker/Habittude Habit Tracker/ContentView.swift
+++ Habittude Habit Tracker/Habittude Habit Tracker/ContentView.swift
@@ -10,23 +10,47 @@ import SwiftUI
 struct ContentView: View {
     @StateObject var habit = Habit()
     
+    @State private var showingAddHabitView = false
+    
     
     var body: some View {
         NavigationView{
             List{
                 Section {
                     ForEach(habit.habits) { habit in
-                        HStack {
+                        NavigationLink {
+                            Text(habit.habitName)
+//                            Spacer()
+                            Text(habit.description)
+                        } label: {
                             Text(habit.habitName)
                         }
                         
                     }
+                    .onDelete(perform: deleteHabit)
                 }
             }
             .navigationTitle("Habittude")
+            .toolbar {
+                Button {
+                    showingAddHabitView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddHabitView) {
+                AddNewHabit(habit: habit)
+            }
         }
     }
+    
+    
+    func deleteHabit(at offset: IndexSet) {
+        habit.habits.remove(atOffsets: offset)
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
