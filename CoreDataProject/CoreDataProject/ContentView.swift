105eba503f4447dfbc91d02f0ca0052ed9846f1e
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-06-23 15:33:38 -0500

    using constraints with CoreData

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -5,22 +5,50 @@
 //  Created by Jumoke Bolanle on 6/20/23.
 //
 
+import CoreData
 import SwiftUI
 
 //MARK: CUSTOM STRUCTS
-struct Student: Hashable {
-    let name: String
-}
+
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     
-    let students = [Student(name: "Rock Lee"), Student(name: "Neji Hyuga")]
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Aliens", "Firefly", "Star Trek"])) var ships: FetchedResults<Ship>
+    
     
     //MARK: CONTENTVIEW
     var body: some View {
-        List(students, id: \.self) { student in
-            Text(student.name)
+        VStack {
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millenium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
+                
+//                do {
+//                    try moc.save()
+//                } catch {
+//                    print(error.localizedDescription)
+//                }
+            }
         }
     }
 }
--- CoreDataProject/CoreDataProject/DataController.swift
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -14,7 +14,10 @@ class DataController: ObservableObject {
         container.loadPersistentStores { description, error in
             if let error = error {
                 print("Core Data failed to laod: \(error.localizedDescription)")
+                return
             }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         }
     }
 }
