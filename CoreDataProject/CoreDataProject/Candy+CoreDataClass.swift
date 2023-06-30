43a485317a698df40aa2759ed14b8ad468a4c091
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-06-29 20:52:02 -0500

    learning about one-to-many relationships in CoreData

---
+++ CoreDataProject/CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Jumoke Bolanle on 6/29/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Jumoke Bolanle on 6/29/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
