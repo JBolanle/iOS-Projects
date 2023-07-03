ea189139ccd8383e82e7e7105d8be7532e7644e3
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-07-03 16:15:26 -0500

    working with confirmationDailog

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.blue
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
+                .onTapGesture {
+                    showingConfirmation = true
+                }
+                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                    Button("Red") { backgroundColor = .red }
+                    Button("Green") { backgroundColor = .green }
+                    Button("White") { backgroundColor = .white }
+                    Button("Cancel", role: .cancel) { }
+                } message: {
+                    Text("Select new color")
+                }
         }
         .padding()
     }
