8f053856396be7ccc6a49971ec1f0b1526a555d0
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-07-23 15:44:55 -0500

    init bucketlist app project

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jumoke Bolanle on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
