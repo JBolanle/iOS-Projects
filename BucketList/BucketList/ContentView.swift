141f9b03c1f84e0cfead091a99a4e597f2345f86
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-08-08 15:20:55 -0500

    using emun to switch states

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    var loadingState = LoadingState.failed
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if loadingState == .loading {
+            LoadingView()
+        } else if loadingState == .success {
+            SuccessView()
+        } else if loadingState == .failed {
+            FailedView()
         }
-        .padding()
+    }
+
+    enum LoadingState {
+        case loading, success, failed
+    }
+
+    struct LoadingView: View {
+        var body: some View {
+            Text("Loading...")
+        }
+    }
+
+    struct SuccessView: View {
+        var body: some View {
+            Text("Success!")
+        }
+    }
+
+    struct FailedView: View {
+        var body: some View {
+            Text("Failed.")
+        }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
