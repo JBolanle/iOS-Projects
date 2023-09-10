23ae4cc5b4361f0df4b5ba47eb583e0d1b09b60e
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-09-10 16:40:38 -0500

    finish accessibility project. init hotprospects

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Jumoke Bolanle on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
