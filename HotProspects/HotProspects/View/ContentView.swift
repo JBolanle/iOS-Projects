9925d8352b86d41844bbadf2347174a4610d4f7a
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-09-25 18:13:23 -0500

    working with image interpolation. set interpolation to none to reduce blur

--- HotProspects/HotProspects/View/ContentView.swift
+++ HotProspects/HotProspects/View/ContentView.swift
@@ -48,37 +48,14 @@ struct DisplayView: View {
 
 
 struct ContentView: View {
-    @StateObject var user = User()
-    @State private var selectedTab = "One"
-
-    @State private var output = ""
-    func fetchReadings() async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings."
-        }
-
-        let result = await fetchTask.result
-
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Error: \(error.localizedDescription)"
-        }
-
-    }
-
-
-    @ObservedObject var updater = DelayedUpdater()
-
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
+        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
     
 }
