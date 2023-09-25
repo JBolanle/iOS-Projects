8b52f8d32377c3452b57978c777137d36e97538d
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-09-25 18:09:26 -0500

    updating data with async result

--- HotProspects/HotProspects/View/ContentView.swift
+++ HotProspects/HotProspects/View/ContentView.swift
@@ -13,6 +13,23 @@ class User: ObservableObject {
     @Published var name = "Taylor Swift"
 }
 
+@MainActor
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
 struct EditView: View {
     @EnvironmentObject var user: User
 
@@ -29,28 +46,39 @@ struct DisplayView: View {
     }
 }
 
+
 struct ContentView: View {
     @StateObject var user = User()
     @State private var selectedTab = "One"
 
-    var body: some View {
-        TabView(selection: $selectedTab) {
-            DisplayView()
-                .onTapGesture {
-                    selectedTab = "Two"
-                }
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-                .tag("One")
-
-            EditView()
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
+    @State private var output = ""
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings."
         }
-        .environmentObject(user)
+
+        let result = await fetchTask.result
+
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
+
+    }
+
+
+    @ObservedObject var updater = DelayedUpdater()
+
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
     }
     
 }
