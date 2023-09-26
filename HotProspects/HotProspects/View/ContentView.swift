7c151e3d157727bd8e338e1fcb251aba9e1ffa62
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-09-26 15:45:38 -0500

    added tab bars with tab items

--- HotProspects/HotProspects/View/ContentView.swift
+++ HotProspects/HotProspects/View/ContentView.swift
@@ -10,15 +10,25 @@ import SwiftUI
 
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
-    }
-
     var body: some View {
-        Text(results)
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .notcontacted)
+                .tabItem {
+                    Label("Not Contacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
     
 }
