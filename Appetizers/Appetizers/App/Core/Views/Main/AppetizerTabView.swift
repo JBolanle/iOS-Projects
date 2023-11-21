f691b32a75de05ea8013204b98a6e95032f948df
Jumoke Bolanle <jumoke.bolanle@gmail.com>
2023-11-21 14:56:08 -0600

    refactor

--- Appetizers/Appetizers/App/Core/Views/Main/AppetizerTabView.swift
+++ Appetizers/Appetizers/App/Core/Views/Main/AppetizerTabView.swift
@@ -9,19 +9,19 @@ import SwiftUI
 
 struct AppetizerTabView: View {
     @EnvironmentObject var order: Order
-    
+
     var body: some View {
         TabView {
             AppetizerListView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
-            
+
             AccountView()
                 .tabItem {
                     Label("Account", systemImage: "person")
                 }
-            
+
             OrderView()
                 .tabItem {
                     Label("Order", systemImage: "bag")
